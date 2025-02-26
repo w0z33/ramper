3236bcaba2a4c58b9c3977175745cd7272aa8f21
Warwick Horton <warwickhorton@hotmail.com>
2025-02-26 11:13:09 +1100

    ramper screen

--- ramper/ContentView.swift
+++ ramper/ContentView.swift
@@ -11,47 +11,28 @@ import SwiftData
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
-
+    
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
+        VStack() {
+            Spacer()
+            Image("Logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(.leading, 60)
+                .padding(.trailing, 60)
+            Text("ramper.co.nz")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.top)
+                .foregroundStyle(Color("Foreground"))
+                .lineLimit(1)
+            Text("Ramper Cricket Scores")
+                .font(.subheadline)
+                .foregroundStyle(Color("Foreground"))
+            Spacer()
         }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
+        .padding()
+        .background(Color("Background"))
     }
 }
 
